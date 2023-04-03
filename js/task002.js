ad13299a26ccb08c41b70b47796a7b2fe0493181
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-03 17:35:30 +0300

    -number
    function opposite(number) {
      //your code here
      return -number
    }

---
+++ js/task002.js
@@ -0,0 +1,18 @@
+/*
+Very simple, given an integer or a floating-point number, find its opposite.
+Examples:
+1: -1
+14: -14
+-34: 34
+*/
+
+function opposite(number) {
+  //your code here
+  return -number
+}
+
+console.log(opposite(1)) // -1
+console.log(opposite(0)) // 0
+console.log(opposite(4.25)) // -4.25
+console.log(opposite(3.3333333)) // -3.3333333
+console.log(opposite(-5)) //5
