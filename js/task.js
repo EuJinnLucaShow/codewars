5fa916478bfb20679dd11735856f24b000cd0e91
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-03 11:06:39 +0300

    Update task.js
    Методи replace() і replaceAll()

---
+++ js/task.js
@@ -0,0 +1,31 @@
+/*Створіть функцію, яка замінює «T» на «U».
+Наприклад:
+"GCAT" => "GCAU"
+Вхідний рядок може бути довільної довжини - зокрема, може бути порожнім. Усі введені дані гарантовано дійсні, тобто кожен вхідний рядок завжди складатиметься лише з «G», «C», «A» та/або «T».*/
+
+// function DNAtoRNA(dna) {
+//   // create a function which returns an RNA sequence from the given DNA sequence
+//     const arrs = dna.split('')
+//     for (let i = 0; i < arrs.length; i++) {
+//         if (arrs[i] === 'T') {
+//             arrs[i] = 'U';
+//         }
+//     }
+//     return arrs.join('');
+// }
+
+// function DNAtoRNA(dna){
+//   return dna.replace(/T/g, 'U');
+// }
+
+const DNAtoRNA = dna => dna.replaceAll('T', 'U');
+
+// function DNAtoRNA(dna) {
+//   return dna.split("T").join("U");
+// }
+
+console.log(DNAtoRNA("GCAT"));       //  "GCAU"
+console.log(DNAtoRNA("TTTT"));       //  "UUUU"
+console.log(DNAtoRNA("GCAT"));       //  "GCAU"
+console.log(DNAtoRNA("GACCGCCGCC")); //  "GACCGCCGCC"
+
