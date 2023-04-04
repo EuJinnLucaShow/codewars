7e6c98bfdc916157f2c3e9ca3c93785a58fb83d4
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-04 13:36:36 +0300

    boolean value
    boolean value

---
+++ js/task008.js
@@ -0,0 +1,13 @@
+/*Implement a function which convert the given boolean value into its string representation.
+Note: Only valid inputs will be given.*/
+
+function booleanToString(b) {
+  return b.toString()
+}
+
+// function booleanToString(b) {
+//   return String(b)
+// }
+
+console.log(booleanToString(true))
+console.log(booleanToString(false))
