316de3c84688988c9a5685322a0079b758cb7abc
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-05 21:10:41 +0300

    Update task014.js

---
+++ js/task014.js
@@ -0,0 +1,9 @@
+/*ES2021 intoduced the numeric separator (_) to make numbers more readable:
+
+Is 1_000_000_000 the same as 1000000000?
+
+true*/
+
+const num1 = 1_000_000_000
+const num2 = 1000000000
+console.log(num1 === num2)
