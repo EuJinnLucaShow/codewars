d2c20eb451cc8632bc571716fe66d59ea14fbb8c
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-09 22:47:09 +0300

    task028

---
+++ js/task028.js
@@ -0,0 +1,14 @@
+/*Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+*/
+
+function solution(string) {
+  return string
+}
+
+console.log(solution('camelCasing')) //'camel Casing'
+console.log(solution('camelCasingTest')) //'camel Casing Test'
