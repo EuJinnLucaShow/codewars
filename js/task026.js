b025093f1bd49f87e77fc3c28a38c064f84342a7
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-09 18:17:33 +0300

    Update task026.js

---
+++ js/task026.js
@@ -0,0 +1,13 @@
+/*When provided with a number between 0-9, return it in words.
+
+Input :: 1
+
+Output :: "One".*/
+
+function switchItUp(number) {
+  //Write your own Code!
+}
+
+console.log(switchItUp(1)) // 'One'
+console.log(switchItUp(3)) // 'Three'
+console.log(switchItUp(5)) // 'Five'
