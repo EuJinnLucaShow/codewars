ee2c03a269d77edecca910973331cc59a0cf875b
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-09 22:40:29 +0300

    task027

---
+++ js/task027.js
@@ -0,0 +1,22 @@
+/*Introduction
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.
+
+Examples
+1705 --> 18
+1900 --> 19
+1601 --> 17
+2000 --> 20
+Note: this kata uses strict construction as shown in the description and the examples, you can read more about it here*/
+
+function century(year) {
+  return Math.ceil(year / 100)
+}
+
+console.log(century(1705)) // 18
+console.log(century(1900)) // 19
+console.log(century(1601)) // 17
+console.log(century(2000)) // 20
+console.log(century(89)) // 1
